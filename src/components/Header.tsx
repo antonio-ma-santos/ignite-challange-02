2c7119e333cb13df8a2017ae4a8a15f08b0714c3
Antônio Marcelino <antonio-dev@hotmail.com>
2022-06-14 20:52:45 -0300

    First commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+
+interface HeaderProps {
+  genreTitle: string;
+}
+
+export function Header({ genreTitle }: HeaderProps) {
+  return (
+    <header>
+      <span className="category">Categoria:<span> {genreTitle}</span></span>
+    </header>
+  );
+}
